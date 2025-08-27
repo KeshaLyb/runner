04ddc29a2ffd1cb17f3a8f53e29abe5cfa6e9809
KeshaLyb <dra4ukilya@yandex.ru>
2025-08-27 19:44:05 +0300

    add controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float horizontal;
+    private float vertical;
+    private int speed = 4;
+    private int force = 5;
+    private Rigidbody rb;
+    private bool onGround;
     void Start()
     {
-        
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         
+
+        transform.Translate(vertical * Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(horizontal * Vector3.right * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            onGround = false;
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("OnGround"))
+        {
+            onGround = true;
+        }
     }
 }
