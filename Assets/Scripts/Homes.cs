73710da9d0e579cf298317f0dc748ca64ac7bee2
KeshaLyb <dra4ukilya@yandex.ru>
2025-09-09 17:46:41 +0300

    add effect infinity

--- Assets/Scripts/Homes.cs
+++ Assets/Scripts/Homes.cs
@@ -7,9 +7,12 @@ public class Homes : MonoBehaviour
     public bool checkDirection = false;
     private int speed = 3;
     private Vector3 direction;
+    public GameObject oneTeleport;
+    public GameObject twoTeleport;
     void Start()
     {
-        
+        oneTeleport = GameObject.FindGameObjectWithTag("OneTeleport");
+        twoTeleport = GameObject.FindGameObjectWithTag("TwoTeleport");
     }
 
     void Update()
@@ -22,5 +25,19 @@ public class Homes : MonoBehaviour
             direction = Vector3.up;
         }
         transform.Translate(direction * speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("DestroyHomes"))
+        {
+            if (checkDirection == false)
+            {
+                gameObject.transform.position = twoTeleport.transform.position;
+            }
+            else
+            {
+                gameObject.transform.position = oneTeleport.transform.position;
+            }
+        }
     }
 }
