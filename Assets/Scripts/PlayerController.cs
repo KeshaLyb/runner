78453cae8be0eb75aa8560f8c38e121947a90b24
KeshaLyb <dra4ukilya@yandex.ru>
2025-09-10 17:47:22 +0300

    add ui and beauties

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController : MonoBehaviour
     private int force = 5;
     private Rigidbody rb;
     private bool onGround;
+    public AudioClip audioJump;
+    private AudioSource audioSource;
     void Start()
     {
         onGround = true;
+        audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && onGround)
         {
             rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            audioSource.PlayOneShot(audioJump);
             onGround = false;
         }
         
